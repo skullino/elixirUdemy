85baf762e7b091f7a7ff8dc76fd37c45923e349c
Eduardo <40169421+skullino@users.noreply.github.com>
2024-06-19 21:15:00 -0300

    -save(deck, filename) -read(filename)

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -20,4 +20,18 @@ defmodule Cards do
     deck = shuffle(deck)
     Enum.split(deck, hand_size)
   end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    {status, binary} = File.read(filename)
+
+    case status do
+      :ok -> :erlang.binary_to_term binary
+      :error -> "That file does not exist"
+    end
+  end
 end
